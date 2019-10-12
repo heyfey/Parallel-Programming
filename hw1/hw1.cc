2eee5b14989c22d2ddbd21826ce1e344b7dcbb29
heyfey PP workstation <tsungtsohsieh@gmail.com>
2019-10-12 21:09:30 +0800

    first A/C

--- hw1/hw1.cc
+++ hw1/hw1.cc
@@ -1,6 +1,98 @@
 #include <cstdio>
 #include <cstdlib>
+#include <algorithm>
+#include <math.h>
 #include <mpi.h>
+
+int find_partner(int phase, int rank, int size){
+    int partner;
+    if(phase % 2 == 0){ // even phase
+        if(rank % 2 == 0){
+            partner = rank + 1;
+        }else{
+            partner = rank - 1;
+        }
+    }else{ // odd phase
+        if(rank % 2 == 0){
+            partner = rank - 1;
+        }else{
+            partner = rank + 1;
+        }
+    }
+    if(partner == -1 || partner == size){
+        partner = MPI_PROC_NULL;
+    }
+    return partner;
+}
+
+void print_array(float *arr, int len){
+    for(int i = 0; i < len; i++){
+        printf(" %f", arr[i]);
+    }
+    printf("\n");
+ }
+
+
+void swap_array(float **a, float **b){
+    float *tmp;
+    tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+int merge_low(float* my_buf, float* recv_buf, float* tmp_buf, int size){
+    int done = 1;
+    int idx;
+    int i = 0, j = 0;
+    for(idx = 0; idx < size; idx++){
+        if(my_buf[i] <= recv_buf[j]){
+            tmp_buf[idx] = my_buf[i];
+            i++;
+        }else{
+            tmp_buf[idx] = recv_buf[j];
+            j++;
+            done = 0;
+        }
+    }
+    return done;
+}
+
+int merge_high(float* my_buf, float* recv_buf, float* tmp_buf, int size){
+    int done = 1;
+    int idx;
+    int i = size - 1;
+    int j = size - 1;
+    for(idx = size-1; idx >= 0; idx--){
+        if(my_buf[i] >= recv_buf[j]){
+            tmp_buf[idx] = my_buf[i];
+            i--;
+        }else{
+            tmp_buf[idx] = recv_buf[j];
+            j--;
+            done = 0;
+        }
+    }
+    return done;
+}
+
+int merge(int phase, int rank, float *my_buf, float *recv_buf, float *tmp_buf, int size){
+    int done = 0;
+    if(phase % 2 == 0){ // even phase
+        if(rank % 2 == 0){
+            done = merge_low(my_buf, recv_buf, tmp_buf, size);
+        }else{
+            done = merge_high(my_buf, recv_buf, tmp_buf, size);
+        }
+    }else{ // odd phase
+        if(rank % 2 == 0){
+            done = merge_high(my_buf, recv_buf, tmp_buf, size);
+        }else{
+            done = merge_low(my_buf, recv_buf, tmp_buf, size);
+        }
+    }
+    return done;
+}
+
 int main(int argc, char** argv) {
     
 	MPI_Init(&argc,&argv);
@@ -8,13 +100,45 @@ int main(int argc, char** argv) {
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
     
+    MPI_Status status;
+    
     int N = atoll(argv[1]);
     int buf_size = N / size;
     int buf_size_last;
     if (N % size == 0){
         buf_size_last = N / size;
     }else{
-        buf_size_last = N % size;
+        buf_size = buf_size + 1;
+        buf_size_last = N % (buf_size);
+    }
+    printf("rank = %d, size = %d, N = %d, buf_size = %d, buf_size_last = %d\n", rank, size, N, buf_size, buf_size_last); 
+
+    // if process count = 1? >= N ?
+    if(size == 1 || size >= N){
+        if(rank == 0){
+            //MPI_File f_in, f_out;
+            FILE *f_in, *f_out;
+            float *buf;
+            buf = new float[N];
+            //MPI_File_open(MPI_COMM_WORLD, argv[2], MPI_MODE_RDONLY, MPI_INFO_NULL, &f_in);
+            //MPI_File_open(MPI_COMM_WORLD, argv[3], MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &f_out);
+            //printf("123\n");
+            f_in = fopen(argv[2], "rb");
+            f_out = fopen(argv[3], "wb");
+            fread(buf, sizeof(float), N, f_in);
+            //MPI_File_read_at(f_in, 0, buf, N, MPI_FLOAT, MPI_STATUS_IGNORE);
+            printf("read done\n");
+            std::sort(buf, buf + N);
+            //print_array(buf, N);
+            fwrite(buf, sizeof(float), N, f_out);
+            //MPI_File_write_at(f_out, 0, buf, N, MPI_FLOAT, MPI_STATUS_IGNORE);
+            printf("write done\n");
+            MPI_Finalize();
+            return 0;
+        }else{
+            MPI_Finalize();
+            return 0;
+        }
     }
 
     //Read file.
@@ -24,26 +148,57 @@ int main(int argc, char** argv) {
     if(rc){
         printf("faied to read file.\n");
     }
+    
+    float *my_buf, *recv_buf, *tmp_buf;
+	my_buf = new float[buf_size];
+    recv_buf = new float[buf_size];
+    tmp_buf = new float[buf_size];
 
-	float * data = new float[buf_size];
-	MPI_File_read_at(f_in, sizeof(float) * rank * buf_size, data, buf_size, MPI_FLOAT, MPI_STATUS_IGNORE);
-	
-    // if only 1 process?
-    // if processes > N ?
+	MPI_File_read_at(f_in, sizeof(float) * rank * buf_size, my_buf, buf_size, MPI_FLOAT, MPI_STATUS_IGNORE);	
 
+    //deal with the last process
+    if(rank == size - 1 && buf_size != buf_size_last){
+        for(int i = buf_size_last; i < buf_size; i++){
+            my_buf[i] = INFINITY;
+        }
+    }
+    
+    //local sort.
+    std::sort(my_buf, my_buf + buf_size);
+    
     //Main work here.
     int phase = 0;
+    int partner;
+    int alldone = 0;
+    int done = 0;
     while(1){
-        if(phase % 2 == 0){ // even phase.
-
-        }else{ // odd phase.
-
-
+        //done = 0;
+        partner = find_partner(phase, rank, size);
+        
+        if(rank % 2 == 0){
+            MPI_Send(my_buf, buf_size, MPI_FLOAT, partner, 0, MPI_COMM_WORLD);
+            MPI_Recv(recv_buf, buf_size, MPI_FLOAT, partner, MPI_ANY_TAG, MPI_COMM_WORLD, &status);    
+        }else{
+            MPI_Recv(recv_buf, buf_size, MPI_FLOAT, partner, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            MPI_Send(my_buf, buf_size, MPI_FLOAT, partner, 0, MPI_COMM_WORLD);
         }
-        //reduce & break....
-        break;
+        
+        //MPI_Sendrecv(my_buf, buf_size, MPI_FLOAT, partner, MPI_ANY_TAG, recv_buf, buf_size, MPI_FLOAT, partner, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+
+        if(partner != MPI_PROC_NULL){
+            done = merge(phase, rank, my_buf, recv_buf, tmp_buf, buf_size);
+            swap_array(&my_buf, &tmp_buf);
+        }
+
+        MPI_Allreduce(&done, &alldone, 1, MPI_FLOAT, MPI_LAND, MPI_COMM_WORLD);
+        
+        if(alldone && phase != 0){
+            break;
+        }
         phase++;
+        //if(phase >= 1000) break;
     }
+    //if(rank == 1) print_array(my_buf, buf_size);
 
 	//Write file.
     MPI_File f_out;
@@ -51,7 +206,11 @@ int main(int argc, char** argv) {
     if(rc){
         printf("open output file failed.\n");
     }
-    MPI_File_write_at(f_out, sizeof(float) * rank * buf_size, data, buf_size, MPI_FLOAT, MPI_STATUS_IGNORE);
-
+    if(rank == size - 1 && buf_size != buf_size_last){
+        MPI_File_write_at(f_out, sizeof(float) * rank * buf_size, my_buf, buf_size_last, MPI_FLOAT, MPI_STATUS_IGNORE);
+    }else{
+        MPI_File_write_at(f_out, sizeof(float) * rank * buf_size, my_buf, buf_size, MPI_FLOAT, MPI_STATUS_IGNORE);
+    }
 	MPI_Finalize();
+    
 }
